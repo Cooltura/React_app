1403bf9504e0a9aff6bab9f088b1ae2f7f4db9dd
Radek <radek.rudziewicz4@wp.pl>
2019-08-20 10:14:18 +0200

    Secound commmit from Feature

--- src/Board.js
+++ src/Board.js
@@ -20,6 +20,7 @@ class Board extends React.Component{
     renderSquare(i){
       return <Square value={this.state.squares[i]}
       onClick={()=>this.handleClick(i)} 
+     
       />;
     }
     render(){
