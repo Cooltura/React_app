e66b05116d0e42ca966969e671afc406ec1c74f6
Radek <radek.rudziewicz4@wp.pl>
2019-08-20 09:59:44 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import Game from './Game';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+<Game />,
+document.getElementById('root')
+);
+//export default App;
